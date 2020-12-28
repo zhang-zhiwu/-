6b2f54a45388eb251b94bb385dba46edc74353af
zhangzhiwu <1923711958@qq.com>
2020-12-28 15:38:25 +0800

    修改完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,38 +9,59 @@ $(function () {
     })
 
     // var form = layui.form
-    const {form} = layui
+    const { form } = layui
     // 通过form.verrify（）函数自定义校验规则
     form.verify({
         // 自定义了一个叫做 pwd 校验规则
         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
         // 校验两次密码是否一致的规则
-        repwd: function(value) {
+        repwd: function (value) {
             // 通过形参拿到的是确认密码框中的内容
             // 还需要拿到密码框中的内容
             // 然后进行一次等于的判断
             // 如果判断失败,则return一个提示消息即可
             var pwd = $('.reg-box [name=password]').val()
             if (pwd !== value) {
-              return '两次密码不一致！'
+                return '两次密码不一致！'
             }
-          }
+        }
     })
-
+    const { layer } = layui
     // 监听注册表单的提交事件
-    $('#form_reg').on('submit',function(e){
+    $('#form_reg').on('submit', function (e) {
         e.preventDefault()
-        var data = {
-            username: $('#form_reg [name=username]').val(),
-            password: $('#form_reg [name=password]').val()
-          }
-        $.post('http://ajax.frontend.itheima.net/api/reguser',data,function(res){
-            console.log(res);
-            if (res.status !== 0){
-                return console.log(res.message||'注册失败');
+        $.ajax({
+            url: '/api/reguser',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('注册成功，请登录！')
+                // 模拟人的点击行为
+                $('#link_login').click()
             }
-            console.log(res.message||'注册成功');
         })
     })
-    
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败')
+                }
+                layer.msg('登录成功')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
 })
